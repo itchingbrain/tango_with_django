bae201c4323b53e78b11340ea9106101ec103bb5
Andrew Kim <itchingbrain@gmail.com>
2017-02-28 23:22:30 +0900

    rango model.py updated

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,20 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    msg = "Rango says hey there!"
-    link = "<a href = /rango/about> About </a>"
-    return HttpResponse(msg + " " + link)
+    # Construct a dictionary to pass to the template engine as its context
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    #Return a rendered response to send to the client
+    # We make use of the shortcut function to make out lives easier
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    msg = "Here is the about page."
-    back = "<a href = />back to main</a>"
-    return HttpResponse(msg + "\n" + back)
+    msg0 = "Here is the about page. Tada!"
+    msg1 = " This file has been composed by Andrew B. Kim"
+    msg = msg0 + msg1
+    context_dict = {'boldmessage': msg}
+    return render(request, 'rango/about.html', context=context_dict)
 
